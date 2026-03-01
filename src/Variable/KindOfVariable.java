5088e4c618e80d273b062114c68b4b194392dcca
Alok Jha <btech60118.19@bitmesra.ac.in>
2026-03-01 16:35:54 +0530

    KindOfVariable

---
+++ src/Variable/KindOfVariable.java
@@ -0,0 +1,25 @@
+package Variable;
+
+import Intro.Employee;
+
+public class KindOfVariable {
+    //member variable
+    int memberVariable;
+
+    KindOfVariable(){
+
+    }
+    //Constructor variable
+    KindOfVariable(int a){
+
+    }
+
+    public static void main(String[] args){
+        byte localVariable = 4; //local variable
+    }
+
+    //Method Variable/constructor
+    public int dummyMenthod(int a ,int b){
+        return a+b;
+    }
+}
