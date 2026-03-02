d73308bfac6df27173f1a8f5b728cb0d9b5a067d
Alok Jha <btech60118.19@bitmesra.ac.in>
2026-03-02 23:49:30 +0530

    Methods

---
+++ src/Methods/intro.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class intro {
+    public static void main(String[] args) {
+        System.out.println(sum(35,32));
+    }
+
+    public static  int sum(int a , int b) {
+        return a+b;
+    }
+}
