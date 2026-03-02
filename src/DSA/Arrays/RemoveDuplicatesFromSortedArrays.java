11e0474bd74815d16b19e0dc77692e9b2f9d9065
Alok Jha <btech60118.19@bitmesra.ac.in>
2026-03-02 23:02:15 +0530

    RemoveDuplicatefromSortedArray

---
+++ src/DSA/Arrays/RemoveDuplicatesFromSortedArrays.java
@@ -0,0 +1,22 @@
+package DSA.Arrays;
+
+public class RemoveDuplicatesFromSortedArrays {
+
+    public static void main(String[] args) {
+       int arr[] = {0,0,1,1,1,2,2,3,3,4};
+
+       int i = 0;
+
+       for(int j = 1 ;j<arr.length;j++){
+           if(arr[i] != arr[j]){
+               i++;
+               arr[i] = arr[j];
+           }
+       }
+
+       for(int k = 0 ; k <= i ;k++){
+           System.out.print(arr[k] + " ");
+       }
+    }
+
+}
