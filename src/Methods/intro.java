646cdd0dedcbb0c35d1f140bd21615f2f49688b2
Alok Jha <btech60118.19@bitmesra.ac.in>
2026-03-04 00:07:11 +0530

    VARAGS

--- src/Methods/intro.java
+++ src/Methods/intro.java
@@ -6,7 +6,7 @@ public class intro {
         System.out.println(sum(35,32));
         int var = 36;
         System.out.println(Math.sqrt(var));
-
+        System.out.println(sum(4,34,5,34,23,24,523,5,2324,52344));
     }
 
     public static  int sum(int a , int b) {
@@ -15,5 +15,13 @@ public class intro {
 
     public static  int sum(int a,int b,int c){
         return a+b+c;
+    }
+
+    public static int sum(int ...variable){
+        int output=0;
+        for(int var :variable){
+            output = output+var;
+        }
+        return output;
     }
 }
