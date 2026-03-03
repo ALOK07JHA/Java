a9ed3879147e4610885b415a0d57206739a26d9f
Alok Jha <btech60118.19@bitmesra.ac.in>
2026-03-03 23:49:41 +0530

    somechanges

--- src/Methods/intro.java
+++ src/Methods/intro.java
@@ -2,10 +2,18 @@ package Methods;
 
 public class intro {
     public static void main(String[] args) {
+
         System.out.println(sum(35,32));
+        int var = 36;
+        System.out.println(Math.sqrt(var));
+
     }
 
     public static  int sum(int a , int b) {
         return a+b;
+    }
+
+    public static  int sum(int a,int b,int c){
+        return a+b+c;
     }
 }
